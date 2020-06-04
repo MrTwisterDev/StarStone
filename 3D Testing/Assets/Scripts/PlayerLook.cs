9cd0393a1f9282c4591ab0ef5208f6ebcb66ee48
SetumupJoeDev <SetumupJoeDev@users.noreply.github.com>
2020-06-04 18:43:14 +0100

    My 3D Proof of Concept project

---
+++ 3D Testing/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+
+    private float mouseX, mouseY, xRotation, zRotation;
+
+    public float mouseSensitivity;
+
+    public Transform playerBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        mouseSensitivity = 100f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        zRotation -= mouseX;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        zRotation = Mathf.Clamp(zRotation, -160f, 160f);
+
+        if (Input.GetKey(KeyCode.LeftAlt))
+        {
+            transform.localRotation = Quaternion.Euler(xRotation, zRotation, 0f);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftAlt))
+        {
+            transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+        else
+        {
+            playerBody.Rotate(Vector3.up * mouseX);
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        }
+    }
+}
---
+++ 3D Testing/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private float gravity;
+    private float xInput, zInput;
+    private float moveSpeed;
+    private float jumpHeight;
+
+    public Transform groundCheck;
+    public float groundDistance;
+    public LayerMask groundMask;
+
+    private bool isGrounded;
+
+    private CharacterController controller;
+
+    private Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gravity = -9.81f;
+        moveSpeed = 10f;
+        jumpHeight = 3f;
+        groundDistance = 0.4f;
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if(isGrounded && Input.GetButtonDown("Jump"))
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        xInput = Input.GetAxis("Horizontal");
+        zInput = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * xInput + transform.forward * zInput;
+
+        controller.Move(move * moveSpeed * Time.deltaTime);
+    }
+}
