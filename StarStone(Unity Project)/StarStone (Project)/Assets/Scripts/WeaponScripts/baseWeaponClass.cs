2275936e43660c70028b95f2a939d5641a945b87
TwisterMr <61789491+TwisterMr@users.noreply.github.com>
2020-06-10 02:58:12 +0100

    New base weapon class
    +Base weapon class added, providing the framework for weapopn implementation.
    = In later commits a script will allow for easy weapon (Hitscan) creation

---
+++ StarStone(Unity Project)/StarStone (Project)/Assets/Scripts/WeaponScripts/baseWeaponClass.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class baseWeaponClass : MonoBehaviour
+{
+    public string WeaponName;
+
+    public int maxBullets; //The maximum amount of uses a weapon has in a single clip
+    public int currentBullets; //The current amount of uses a weapon has in it's current clip
+
+    public bool canShoot; //The weapon can be used, set to false whenever the player runs out of currentBullets
+
+    public bool canADS; //The gun does have the ability to ADS (Aim down sights)
+    public float weaponSpeedMultiplier; //How much a gun slows the character down when equipped (0 has no effect)
+
+
+
+    void Update()
+    {
+        if(currentBullets <= 0)
+        {
+            canShoot = false;
+        }
+    }
+
+    // Update is called once per frame
+    public virtual void useWeapon()
+    {
+        currentBullets--;
+    }
+}
