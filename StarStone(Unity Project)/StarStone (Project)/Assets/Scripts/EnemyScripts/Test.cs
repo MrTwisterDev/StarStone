73f47b564acf18e7f5a8d8c1ea3b4c44cbb82a1b
TwisterMr <61789491+TwisterMr@users.noreply.github.com>
2020-06-13 16:37:41 +0100

    Navmesh test
    +Added test script for figuring out pathfinding
    ~Changed render setting to a higher AA level

---
+++ StarStone(Unity Project)/StarStone (Project)/Assets/Scripts/EnemyScripts/Test.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Test : MonoBehaviour
+{
+
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<NavMeshAgent>().destination = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
