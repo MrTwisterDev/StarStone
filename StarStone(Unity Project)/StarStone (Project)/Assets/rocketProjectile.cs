1226e27b689a1cc9293d05393efedacca49e3045
TwisterMr <61789491+TwisterMr@users.noreply.github.com>
2020-07-09 14:52:43 +0100

    Rocket Added

--- StarStone(Unity Project)/StarStone (Project)/Assets/rocketProjectile.cs
+++ StarStone(Unity Project)/StarStone (Project)/Assets/rocketProjectile.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class rocketProjectile : MonoBehaviour
 {
+    public float rocketSpeed;
+    public float angularSpeed;
+
+    public GameObject targetedPlayer;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    public void startFromEnemy()
+    {
+
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(targetedPlayer.transform);
+        transform.Translate(transform.forward * rocketSpeed);
     }
 }
