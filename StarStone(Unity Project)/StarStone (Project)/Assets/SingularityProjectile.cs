0c0d78579e42b49fe51d388fe125d31a8656fb1e
SetumupJoeDev <SetumupJoeDev@users.noreply.github.com>
2020-07-15 18:02:35 +0100

    Sounds & Singularity
    -Began working on the Singularity fire mode of the PW
    -Created a PW projectile base class
    -Added firing sounds to the PW

---
+++ StarStone(Unity Project)/StarStone (Project)/Assets/SingularityProjectile.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingularityProjectile : PrototypeProjectileBase
+{
+    public float fuseLength;
+    private bool hasDetonated;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(fuseLength == 0) { fuseLength = 3f; }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasDetonated)
+        {
+            fuseLength -= Time.deltaTime;
+            if(fuseLength <= 0)
+            {
+                hasDetonated = true;
+                rigidBody.isKinematic = false;
+            }
+        }
+        else
+        {
+            Detonate();
+        }
+    }
+
+    private void Detonate()
+    {
+        
+    }
+
+}
