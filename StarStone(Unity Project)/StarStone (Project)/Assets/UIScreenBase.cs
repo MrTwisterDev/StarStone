7f19f71c26c3d3f5e64b640ea2bad210a2e8b46c
TwisterMr <61789491+TwisterMr@users.noreply.github.com>
2020-07-21 17:59:36 +0100

    Menu Additions

---
+++ StarStone(Unity Project)/StarStone (Project)/Assets/UIScreenBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIScreenBase : MonoBehaviour
+{
+    public menuSwitcher menuController;
+    public GameObject screenToChange;
+
+ 
+
+
+}
