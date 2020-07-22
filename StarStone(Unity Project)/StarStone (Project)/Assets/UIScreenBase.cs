1a86f8ff54c08b0a85780b65a9dd8eb8a4b3ac86
TwisterMr <61789491+TwisterMr@users.noreply.github.com>
2020-07-22 01:41:55 +0100

    Menu Complete
    +Super dynamic options allowing lewis to customise the selectable options by just editing what the options do.
    
    +Cleanup and general aesthetics
    +Fullscreen, resolution and AA options added

--- StarStone(Unity Project)/StarStone (Project)/Assets/UIScreenBase.cs
+++ StarStone(Unity Project)/StarStone (Project)/Assets/UIScreenBase.cs
@@ -7,7 +7,10 @@ public class UIScreenBase : MonoBehaviour
     public menuSwitcher menuController;
     public GameObject screenToChange;
 
- 
+    public void changeMenu()
+    {
+        menuController.switchScreen(screenToChange);
+    }
 
 
 }
