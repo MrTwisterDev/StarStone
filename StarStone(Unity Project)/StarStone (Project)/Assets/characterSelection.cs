2e3034fd45b839f793dfa713439ee558fa49a6be
TwisterMr <61789491+TwisterMr@users.noreply.github.com>
2020-07-29 04:22:50 +0100

    Co-op Menu Addition
    +New co op menu!
    Selectable characters
    Semi designer friendly!
    #Needs to implement changable characters in the 3D viewer
    #Needs to be hooked up with Jame's systems

---
+++ StarStone(Unity Project)/StarStone (Project)/Assets/characterSelection.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class characterSelection : MonoBehaviour
+{
+    public GameObject[] characterPrefabs;
+
+    public TMP_Dropdown[] playerSelectDropDowns = new TMP_Dropdown[2];
+
+    public Transform[] playerSelectShowCaseTransforms = new Transform[2];
+
+    private GameObject[] shownPlayers = new GameObject[2];
+    //public showCaseScript;
+    void Start()
+    {
+        for (int i = 0; i < shownPlayers.Length; i++)
+        {
+            shownPlayers[i] = Instantiate(characterPrefabs[playerSelectDropDowns[i].value], playerSelectShowCaseTransforms[i].position, characterPrefabs[playerSelectDropDowns[i].value].transform.rotation,playerSelectShowCaseTransforms[i]);
+
+            for (int a = 0; a < playerSelectDropDowns[i].options.Count; a++)
+            {
+                playerSelectDropDowns[i].options[a].text = characterPrefabs[a].name;
+            }
+        }
+
+
+    }
+
+    private void Update()
+    {
+
+
+
+    }
+
+}
