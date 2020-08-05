cd8cccb9f6ee0c818bf2934ca150f05080e83f28
SetumupJoeDev <SetumupJoeDev@users.noreply.github.com>
2020-08-05 22:27:04 +0100

    Tutorial Controller Finished!
    +Finished work on the tutorial controller
    +Added a tutorial button to the main menu canvas

--- StarStone(Unity Project)/StarStone (Project)/Assets/Scripts/Misc/TutorialTextController.cs
+++ StarStone(Unity Project)/StarStone (Project)/Assets/Scripts/Misc/TutorialTextController.cs
@@ -11,11 +11,14 @@ public class TutorialTextController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Calculates the object's current distance from the player
         float distanceFromPlayer = Vector3.Distance(gameObject.transform.position, playerObject.transform.position);
+        //If the player is 5 meters away or closer and the text is currently disabled, the text object is set to active
         if(distanceFromPlayer <= 5f && !textObject.activeSelf)
         {
             textObject.SetActive(true);
         }
+        //If the player is further than 5 meters away and the text is active, it is disabled
         else if(distanceFromPlayer > 5f && textObject.activeSelf)
         {
             textObject.SetActive(false);
